76971bde51afda32b5a3a0fb09d141010c639030
Edward Adeyemi <edwardadeyemi@Edwards-MacBook-Air.local>
2024-11-10 21:03:12 +0100

    Deploy ERC721 Token

---
+++ scripts/mint.js
@@ -0,0 +1,27 @@
+// Import necessary modules from Hardhat and SwisstronikJS
+const hre = require("hardhat");
+const { SwisstronikPlugin } = require("@swisstronik/web3-plugin-swisstronik");
+hre.web3.registerPlugin(new SwisstronikPlugin(hre.network.config.url));
+async function main() {
+    const replace_contractAddress = "0xD43558397C29c400bb65ff66Bc1106AB69ccfD2c";
+    const [from] = await hre.web3.eth.getAccounts();
+    const contractFactory = await hre.ethers.getContractFactory("TestNFT");
+    const ABI = JSON.parse(contractFactory.interface.formatJson());
+    const contract = new hre.web3.eth.Contract(ABI, replace_contractAddress);
+    const replace_functionArgs = "0xec56892333a883Db3a1BE2E5F1aC0ba541b1D80F"; // Recipient address
+    console.log("Minting 1 token...");
+    try {
+        const transaction = await contract.methods.safeMint(replace_functionArgs).send({ from });
+        console.log("Transaction submitted! Transaction details:", transaction);
+        // Display success message with recipient address
+        console.log(`Transaction completed successfully! ✅ Non-Fungible Token minted to ${replace_functionArgs}`);
+        console.log("Transaction hash:", transaction.transactionHash);
+    } catch (error) {
+        console.error(`Transaction failed! Could not mint NFT.`);
+        console.error(error);
+    }
+}
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
